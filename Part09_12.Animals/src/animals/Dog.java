c47a3ea833725125e64817a75e31c79fd20669ea
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-12-23 21:57:37 -0300

    add: Animals

---
+++ Part09_12.Animals/src/animals/Dog.java
@@ -0,0 +1,22 @@
+package animals;
+
+public class Dog extends Animal implements NoiseCapable {
+
+	public Dog(String name) {
+		super(name);
+	}
+
+	public Dog() {
+		super("Dog");
+	}
+
+	public void bark() {
+		System.out.println(getName() + " barks");
+
+	}
+
+	@Override
+	public void makeNoise() {		
+		bark();
+	}
+}
---
+++ Part09_12.Animals/src/animals/NoiseCapable.java
@@ -0,0 +1,7 @@
+package animals;
+
+public interface NoiseCapable {
+	
+	public void makeNoise();
+	
+}
